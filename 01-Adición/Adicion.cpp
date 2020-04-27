e10915b4efeeb1b4969aa1c446066bb8334ee1fb
santillanso <63689956+santillanso@users.noreply.github.com>
2020-04-27 17:45:05 -0300

    Add files via upload

---
+++ 01-Adición/Adicion.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, s;
+    cout << "ingresar primer numero: ";
+    cin >> num1;
+    cout << "ingresar segundo numero: ";
+    cin >> num2;
+
+    s = num1 + num2;
+
+    cout <<"la suma es: "<<s;
+system("pause");
+return 0;
+
+
+
+}
